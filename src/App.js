0c0b8cc107b9a623043ae51ae03ebe378f20224c
dhaguen <david.haguenauer@gmail.com>
2022-09-14 12:02:28 +0200

    Fix warning : minor : pass loose equality to false

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   handleSubmit = (e, history, searchInput) => {
     e.preventDefault();
 
-    if (e.type == "submit")
+    if (e.type === "submit")
     {
       e.currentTarget.reset();
     }
